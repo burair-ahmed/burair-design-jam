49be5899fcfa6a53ffc46022b428cad53268d57f
burair-ahmed <burair.s3380@gmail.com>
2025-01-07 18:05:47 +0500

    `Refactored ShopFilter component layout and content`

--- src/app/components/Shop-Filter/index.tsx
+++ src/app/components/Shop-Filter/index.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
 
 export default function ShopFilter() {
-
-
-    return (
+  return (
     <div className="bg-[#F9F1E7] py-8">
       <div className="grid grid-cols-10 w-[90%] gap-2 mx-auto flex items-center justify-center">
         <div className="col-span-10 md:col-span-1">
@@ -43,47 +41,43 @@ export default function ShopFilter() {
           </div>
         </div>
         <div className="col-span-10 md:col-span-2 md:border-l-2 md:border-[#9F9F9F] flex justify-center items-center pl-6">
-        <h1 className="text-[16px]">Showing 1–16 of 32 results</h1>
+          <h1 className="text-[16px]">Showing 1–16 of 32 results</h1>
         </div>
-        <div className="col-span-4">
-
-        </div>
+        <div className="col-span-4"></div>
 
         <div className="col-span-10 md:col-span-3">
-            <div className="grid grid-cols-12">
-
-        
-        <div className="col-span-6 md:col-span-1">
-        <div className="grid grid-cols-12 gap-8">
+          <div className="grid grid-cols-12">
+            <div className="col-span-6 md:col-span-3">
+              <div className="grid grid-cols-12 gap-8">
                 <div className="col-span-2 flex justify-start items-center">
                   <h1 className="text-[20px]">Show</h1>
                 </div>
                 <div className="col-span-8 flex ">
-                <select name="" id="" className="text-md custom-select" >
-                        <option value="08">08</option>
-                        <option value="16">16</option>
-                        <option value="24">24</option>
-                        <option value="32">32</option>
-                    </select>
+                  <select name="" id="" className="text-md custom-select">
+                    <option value="08">08</option>
+                    <option value="16">16</option>
+                    <option value="24">24</option>
+                    <option value="32">32</option>
+                  </select>
                 </div>
               </div>
-        </div>
-        <div className="col-span-6 md:col-span-1">
-        <div className="grid grid-cols-12">
+            </div>
+            <div className="col-span-6 md:col-span-6">
+              <div className="grid grid-cols-12">
                 <div className="col-span-5 md:col-span-3 items-center">
                   <h1 className="text-[20px]">Sort By</h1>
                 </div>
                 <div className="col-span-2 flex ">
-                <select name="" id="" className="text-md custom-select" >
-                        <option value="08">08</option>
-                        <option value="16">16</option>
-                        <option value="24">24</option>
-                        <option value="32">32</option>
-                    </select>
+                  <select name="" id="" className="text-md custom-select">
+                    <option value="08">08</option>
+                    <option value="16">16</option>
+                    <option value="24">24</option>
+                    <option value="32">32</option>
+                  </select>
                 </div>
               </div>
-              </div>
-        </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
