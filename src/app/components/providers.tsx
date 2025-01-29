a4a9a963d4794098db40740d36623ec17ca22055
burair-ahmed <burair.s3380@gmail.com>
2025-01-29 16:13:25 +0500

    Error Resolve

--- src/app/components/providers.tsx
+++ src/app/components/providers.tsx
@@ -1,23 +1,21 @@
 'use client';
 
-import { CartProvider as USCProvider } from "use-shopping-cart";
-import { ReactNode } from "react";
-
-export default function CartProvider({ children }: { children: ReactNode }) {
-  return (
-    <div>
-      <USCProvider
-        mode="payment"
-        cartMode="client-only"
-        successUrl="http://localhost:3000/success"
-        cancelUrl="http://localhost:3000/error"
-        currency="PKR"
-        billingAddressCollection={true}
-        shouldPersist={true}
-        language="en-US"
-      >
-        {children}
-      </USCProvider>
-    </div>
-  );
+import {CartProvider as USCProvider} from "use-shopping-cart"
+import {ReactNode} from "react";
+export default function CartProvider({children}: {children: ReactNode}) {
+    return (
+        <div>
+            <USCProvider
+            mode="payment"
+            cartMode="client-only"
+            successUrl="http://localhost:3000/success"
+            cancelUrl="http://localhost:3000/error"
+            currency="PKR"
+            billingAddressCollection={true}
+            shouldPersist={true}
+            language="en-US">
+                {children}
+            </USCProvider>
+        </div>
+    );
 }
